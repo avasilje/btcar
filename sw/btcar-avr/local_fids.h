f1d6addba99be4648f3921c8253c20d67e693a2a
avasilje <solit@bk.ru>
2014-08-21 01:05:44 +0300

    BTCAR-AVR: Move DBG buff functionality to dedicated files

---
+++ sw/btcar-avr/local_fids.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ *  
+ *
+ *  FILE
+ *    local_fids.h
+ *
+ *  DESCRIPTION
+ *    
+ *
+ ******************************************************************************/
+
+#ifndef __LOCAL_FIDS_H__
+#define __LOCAL_FIDS_H__
+
+#define FID_MAIN        1
+#define FID_DBG_LOG     2
+#define FID_ACTIONS     3
+
+
+#endif // __DBG_LOG_H__
